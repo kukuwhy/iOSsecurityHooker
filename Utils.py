5ff827c7963c80a4c636f9575fc1578176d6dc3b
ch3ony <ch3ony@gmail.com>
2022-01-26 16:43:21 +0900

    first

--- Utils.py
+++ Utils.py
@@ -1,11 +1,26 @@
-import re
+from email.mime import application
 import frida
+import string
+import random
+import socket
 from PyInquirer import prompt, style_from_dict, Token
+import colorama
+from termcolor import *
+import platform
+import re
 
 LOGO = """
 
 
 """
+
+targetName = "com.highaltitudehacks.DVIAswiftv2"
+
+# open hook script
+jail_script = open('./script/jailBreak.js', 'r').read()
+anti_script = open('./script/antiDebugging.js', 'r').read()
+ssl_script = open('./script/sslPinning.js', 'r').read()
+
 
 style = style_from_dict({
     Token.Separator: '#cc5454',
@@ -17,10 +32,48 @@ style = style_from_dict({
     Token.Question: '',
 })
 
-targetName = "com.highaltitudehacks.DVIAswiftv2"
-jail_script = open('./script/jaildetect.js', 'r').read()
-anti_script = open('./script/antiDebugging.js', 'r').read()
-ssl_script = open('./script/sslPinning.js', 'r').read()
+def printInputPrompt(name, message):
+    questions = [
+        {
+            'type': 'input',
+            'name': name,
+            'message': message,
+        }
+    ]
+    pmt = prompt(questions, style=style, qmark='[?]')[name]
+    return pmt
+
+def printListPrompt(name, message, list):
+    questions = [
+        {
+            'type': 'list',
+            'name': name,
+            'message': message,
+            'choices' : list
+        }
+    ]
+    pmt = prompt(questions, style=style, qmark='[?]')[name]
+    return pmt
+
+
 
 def getDevice():
     return frida.get_usb_device(1)
+
+def getApplist():
+    device = getDevice()
+    apps = device.enumerate_applications()
+    list = []
+    for app in apps :
+        print(app.name)
+        list.append({'name':app.name,'package':app.identifier})
+
+    targetName = []
+    print(list)
+    targetName = printListPrompt('package', 'Choose the paacad :', list)
+    print(targetName)
+
+
+
+if __name__ == "__main__":
+    getApplist()
--- main.py
+++ main.py
@@ -2,19 +2,46 @@ import frida
 import sys
 import Utils as ut
 
+def init():
+    ut.colorama.init()
+
+    # logo printing
+    ut.cprint(ut.LOGO, 'yellow')
+
+    # select devices
+    #device = ut.printListPrompt('DEVICES', 'Select device :', ut.getDevice())
+
+    # auto select device
+    device = ut.getDevice()
+
+    # select target APP
+    targetName = ut.printListPrompt('APP', 'Select target APP :', )
 
 def main():
+    
+    
+
+    targetName = ut.printListPrompt
+
     try:
-        print("hello?")
-        targetDevice = ut.getDevice()
-        targetPid = targetDevice.spawn(ut.targetName)
-        session = targetDevice.attach(targetPid)
+        # search iPhone device
+        device = ut.getDevice()
+
+        # spawn and attach target APP
+        targetPid = device.spawn(ut.targetName)
+        session = device.attach(targetPid)
+
+        # hook script load
         script = session.create_script(ut.jail_script)
         script.load()
-        targetDevice.resume(targetPid)
+
+        # target APP resume
+        device.resume(targetPid)
+
         sys.stdin.read()
     except Exception as e:
         print(e)
 
 if __name__ == "__main__":
+    init()
     main()
